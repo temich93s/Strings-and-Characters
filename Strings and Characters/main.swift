47d4208d8de7ec170222d0ba012e047edf5c9dd5
Temich93s <nil>
2021-09-18 19:43:37 +1000

    Добавил print в раздел строковые литералыю Сделано: Многострочные литералы строк

--- Strings and Characters/main.swift
+++ Strings and Characters/main.swift
@@ -9,9 +9,21 @@ import Foundation
 
 print("Hello, World!")
 
+
 //MARK: Строковые литералы
 print("\n//Строковые литералы")
 
 let someString = "hello world"
+print(someString)
 
 
+//MARK: Многострочные литералы строк
+print("\n//Многострочные литералы строк")
+
+let someString2 = """
+    строка на 1 строке \
+    строка то же на 1 строке (из-за \\)
+        строка на 2 строке, но с 4 отступами
+    строка на 3 строке
+    """
+print(someString2)
